aea7ea4c39c4919b253dad4a5507d28f48cca66e
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-03-03 18:43:57 +0100

    Add favicon

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
-// This is the main.js file. Import global CSS and scripts here.
-// The Client API can be used here. Learn more: gridsome.org/docs/client-api
-
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function(Vue, { router, head, isClient }) {
-    // Set default layout as a global component
     Vue.component('Layout', DefaultLayout)
+
+    head.link.push({
+        name: 'shortcut icon',
+        href: '/favicon.ico',
+        type: 'image/x-icon'
+    })
+
+    head.link.push({
+        name: 'icon',
+        href: '/favicon.ico',
+        type: 'image/x-icon'
+    })
 }
