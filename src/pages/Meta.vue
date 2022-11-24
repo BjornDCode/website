801fc270f66e48024175767cdbd79593e9dcb1bf
Bjørn Lindholm <bjornlindholmhansen@gmail.com>
2022-11-24 02:28:44 +0100

    Update Meta

--- src/pages/Meta.vue
+++ src/pages/Meta.vue
@@ -1,19 +1,25 @@
 <template>
     <Layout>
-        <h1 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 mb-4">About</h1>
+        <h1
+            class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 mb-4"
+        >
+            About
+        </h1>
         <p class="text-gray-700">
-            I'm Bjørn Lindholm from Copenhagen, Denmark. Primarily a front end developer who also dabbles in design and backend work. Currently working on
-            <a
-                href="https://www.branchci.com/"
-            >Branch</a>, a CI/CD tool for Wordpress.
+            I'm Bjørn Lindholm from Copenhagen, Denmark. Primarily a front end
+            developer who also dabbles in design and backend work. Currently
+            building
+            <a href="https://www.reform.app/" class="underline" target="_blank"
+                >Reform</a
+            >, a modern form builder.
         </p>
     </Layout>
 </template>
 
 <script>
-    export default {
-        metaInfo: {
-            title: 'About',
-        },
-    }
+export default {
+    metaInfo: {
+        title: "About",
+    },
+};
 </script>
