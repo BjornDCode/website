4e7c8bbfbefe3afd5c78151f62730c75f2d34dc9
Bjørn Lindholm <bjornlindholmhansen@gmail.com>
2022-11-24 02:48:56 +0100

    Remove highlight.js temporarily

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,43 +3,43 @@
 </template>
 
 <script>
-    import highlight from 'highlight.js/lib/highlight'
-    import javascript from 'highlight.js/lib/languages/javascript'
-    import xml from 'highlight.js/lib/languages/xml'
-    import css from 'highlight.js/lib/languages/css'
-    import php from 'highlight.js/lib/languages/php'
-    import 'highlight.js/styles/github.css'
+// import highlight from "highlight.js/lib/highlight";
+// import javascript from "highlight.js/lib/languages/javascript";
+// import xml from "highlight.js/lib/languages/xml";
+// import css from "highlight.js/lib/languages/css";
+// import php from "highlight.js/lib/languages/php";
+// import "highlight.js/styles/github.css";
 
-    highlight.registerLanguage('javascript', javascript)
-    highlight.registerLanguage('xml', xml)
-    highlight.registerLanguage('css', css)
-    highlight.registerLanguage('php', php)
+// highlight.registerLanguage("javascript", javascript);
+// highlight.registerLanguage("xml", xml);
+// highlight.registerLanguage("css", css);
+// highlight.registerLanguage("php", php);
 
-    export default {
-        props: {
-            string: {
-                type: String,
-                required: true,
-            },
+export default {
+    props: {
+        string: {
+            type: String,
+            required: true,
         },
+    },
 
-        watch: {
-            string: {
-                immediate: true,
-                handler() {
-                    this.$nextTick(() => {
-                        highlight.initHighlighting.called = false
-                        highlight.initHighlighting()
-                    })
-                },
-            },
-        },
-    }
+    // watch: {
+    //     string: {
+    //         immediate: true,
+    //         handler() {
+    //             this.$nextTick(() => {
+    //                 highlight.initHighlighting.called = false
+    //                 highlight.initHighlighting()
+    //             })
+    //         },
+    //     },
+    // },
+};
 </script>
 
 <style>
-    .prose pre {
-        color: theme('colors.gray.800');
-        background-color: theme('colors.gray.100');
-    }
+.prose pre {
+    color: theme("colors.gray.800");
+    background-color: theme("colors.gray.100");
+}
 </style>
